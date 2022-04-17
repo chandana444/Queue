4ee74fd4843403884f10c271746c6ce62e32fa8f
chandana444 <72591703+chandana444@users.noreply.github.com>
2022-04-17 20:02:34 +0530

    Add files via upload

---
+++ header_files/Queue.h
@@ -0,0 +1,27 @@
+#ifndef QUEUE_H_INCLUDED
+#define QUEUE_H_INCLUDED
+#include <iostream>
+using namespace std;
+
+class Queue{
+    private:
+        int arr2[5];
+        int Front;
+        int rear;
+
+    public:
+        Queue();
+
+        bool isEmpty();
+
+        bool isFull();
+
+        void enqueue(int );
+
+        int dequeue();
+
+        int Count();
+
+        void display();
+};
+#endif
